69d0d34b052d28bc9a54e4942ecd2ea00f9a8645
kolpakov <kolpakov@168robotics.com>
2025-08-05 20:42:38 +0300

    Fixed issues with PhysX linking and windows vulkan SDK

--- backends/VelixVK/include/VelixVK/VulkanContext.hpp
+++ backends/VelixVK/include/VelixVK/VulkanContext.hpp
@@ -7,14 +7,6 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#if defined(_WIN32)
-    #define GLFW_EXPOSE_NATIVE_WIN32
-    #include <GLFW/glfw3native.h>
-#elif defined(__linux__)
-    #define GLFW_EXPOSE_NATIVE_X11
-    #include <GLFW/glfw3native.h>
-#endif
-
 #include <vector>
 
 #include "VelixFlow/RenderAPI/Interface/IRenderContext.hpp"
